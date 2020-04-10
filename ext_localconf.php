a4bf114670c19542575d98ca710c5f334ae2c38b
Georg Ringer <georg.ringer@gmail.com>
2020-04-10 05:23:02 +0200

    [TASK] Support 9

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,19 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer::class] = [
-    'className' => \StudioMitte\Urlguard2\Xclass\ContentObjectRendererXclassed::class,
-];
+$boot = static function () {
+    $typo3version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+    if ($typo3version->getMajorVersion() === 9) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer::class] = [
+            'className' => \StudioMitte\Urlguard2\Xclass\ContentObjectRenderer9Xclassed::class,
+        ];
+    } else {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer::class] = [
+            'className' => \StudioMitte\Urlguard2\Xclass\ContentObjectRenderer10Xclassed::class,
+        ];
+    }
+};
+
+$boot();
+unset($boot);
+
