3e08b9d30af6b515b885ef1c0e2c554720073335
Georg Ringer <georg.ringer@gmail.com>
2020-05-07 10:29:41 +0200

    [BUGFIX] CHeck if typo3version class exists

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,14 @@
 defined('TYPO3_MODE') or die();
 
 $boot = static function () {
-    $typo3version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
-    if ($typo3version->getMajorVersion() === 9) {
+    if (class_exists(\TYPO3\CMS\Core\Information\Typo3Version::class)) {
+        $typo3version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+        $version = $typo3version->getMajorVersion();
+    } else {
+        $typo3VersionSplit = explode('.', TYPO3_branch);
+        $version = (int)$typo3VersionSplit[0];
+    }
+    if ($version === 9) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer::class] = [
             'className' => \StudioMitte\Urlguard2\Xclass\ContentObjectRenderer9Xclassed::class,
         ];
@@ -16,4 +22,3 @@ $boot = static function () {
 
 $boot();
 unset($boot);
-
