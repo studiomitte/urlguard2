e18c518a33a92b83f4cc75fedf01e4f1a7dc1014
Georg Ringer <georg.ringer@gmail.com>
2020-05-07 10:08:31 +0200

    [TASK] Lower requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Remove stuff from addquerystring',
-    'description' => '',
+    'title' => 'Remove unwanted params from addquerystring',
+    'description' => 'Just keep all parameters which have been mapped in the routing configuration',
     'category' => 'fe',
     'author' => 'Georg Ringer',
     'author_email' => 'gr@studiomitte.com',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.16-10.4.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
